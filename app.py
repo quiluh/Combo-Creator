690d0f9b3a9d231de89ea2093add7d616b0484f8
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-03-29 16:43:50 +1300

    added home page directory

--- app.py
+++ app.py
@@ -2,5 +2,13 @@ from flask import Flask, render_template, request, jsonify, redirect
 
 app = Flask(__name__)
 
+@app.route("/index")
+def Home():
+    return render_template("index.html")
+
+@app.route("/")
+def PortHome():
+    return redirect("/index")
+
 if __name__ == "__main__":
     app.run()
