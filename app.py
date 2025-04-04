7e6d67cdfaa0f6109dd1521ebf008ebd2e5e59cb
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-04-04 14:17:28 +1300

    imported json and changed name for home directory

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify, redirect
 
+import json
+
 app = Flask(__name__)
 
 @app.route("/index")
-def Home():
+def Index():
     return render_template("index.html")
 
 @app.route("/")
