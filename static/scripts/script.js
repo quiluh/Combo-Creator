39c6bb8cba85789d76a6058c87d98345c40e0715
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-04-04 14:17:44 +1300

    created combo data spine

---
+++ static/scripts/script.js
@@ -0,0 +1,16 @@
+var currentID = 1;
+
+function implement(id) {
+    let data = {
+        leftControl: null,
+        leftShift: null,
+        leftAlt: null,
+
+        inputText:"",
+        outputText:"",
+
+        rightControl: null,
+        rightShift: null,
+        rightAlt: null,
+    }
+}
